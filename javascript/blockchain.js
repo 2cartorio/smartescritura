aae860d83fda57664248d55196d18033a924af3d
2cartorio <58342660+2cartorio@users.noreply.github.com>
2019-11-29 20:49:41 -0300

    Update blockchain.js

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -22,6 +22,24 @@ function valorDepositado() {
 }
 
 
+function valorImovel() {    
+    var boxValorImovel = document.getElementById("boxValorImovel");
+    console.log("valorImovel - submitting the request");     
+    contract.valorImovel()
+    .then( (resultFromContract) => {
+        console.log("valorImovel - result is", resultFromContract);
+        boxValorImovel.innerHTML = resultFromContract;
+    })
+    .catch( (err) => {
+        console.error(err);
+        alert("A screen will be load asking to allow this page to connect with your Ethereum account.\nPlease give this permission to proceed.\nOr if you don't have an Ethereum account please install Metamask");
+        ethereum.enable();
+        alert("After you give the permission we are going to reload the page");
+        document.location = "index.html";
+    });
+}
+
+
 
 function executePayment() {
     var amount = document.frmPayment.amount.value;       
