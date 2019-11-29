c7eba58a5787e9958d4e0f2a66bc46d05c5466ad
2cartorio <58342660+2cartorio@users.noreply.github.com>
2019-11-29 18:22:21 -0300

    Update blockchain.js

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,4 +1,4 @@
-var contractAddress = "0x557D0Ecd8A9ae210519d1bD9B543723E04941eEe";
+var contractAddress = "0x6a6AF8E449690Ca24eFB9ba57aa284B503540a5a";
 var provider = new ethers.providers.Web3Provider(web3.currentProvider);
 var signer = provider.getSigner();
 var contract = new ethers.Contract(contractAddress, contractAbi, signer);
